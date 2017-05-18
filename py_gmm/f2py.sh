f4b6db6d6cdff4f7d1976c5f4e2cf3cfa4e61657
Giovanni Pellegrini <giovi.pelle@gmail.com>
2017-05-18 18:51:02 +0200

    f2py.sh: f2py3 substituted with f2py AuAg_dimer.ipynb: polished

--- py_gmm/f2py.sh
+++ py_gmm/f2py.sh
@@ -1,2 +1,2 @@
-f2py3 --overwrite-signature -m gmm_py -h gmm_py.pyf gmm_f2py_module.f90
-f2py3 -c -lopenblas --build-dir ./tmp --f90exec=/usr/bin/gfortran --fcompiler=gnu95 --f90flags='-O3 -fpic -m64' gmm_py.pyf kinds.f90 datatypes.f90 operators.f90 shared_data.f90 basicsubs.f90 gmmsubs.f90 sing_part.f90 vec_trans.f90 local_field.f90 linear_solver.f90 gmm_f2py_module.f90
+f2py --overwrite-signature -m gmm_py -h gmm_py.pyf gmm_f2py_module.f90
+f2py -c -lopenblas --build-dir ./tmp --f90exec=/usr/bin/gfortran --fcompiler=gnu95 --f90flags='-O3 -fpic -m64' gmm_py.pyf kinds.f90 datatypes.f90 operators.f90 shared_data.f90 basicsubs.f90 gmmsubs.f90 sing_part.f90 vec_trans.f90 local_field.f90 linear_solver.f90 gmm_f2py_module.f90
